a0bb6bacb6cb644f53766e3ecc78a3a3fc91a1fe
Charidimos Papadakis <harrypapadakis02@gmail.com>
2024-09-18 19:41:07 +0300

    Delete .history directory

--- .history/llvm_new/alan_20240818175829.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./alanc < $1 > a.ll
-llc -mtriple=aarch64-apple-macosx11.0.0 -o a.s a.ll
-clang -o a.out a.s lib/lib.a -no-pie
--- .history/llvm_new/alan_20240818180156.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./alanc < $1 > a.ll
-llc -o a.s a.ll
-clang -o a.out a.s lib/lib.a 
